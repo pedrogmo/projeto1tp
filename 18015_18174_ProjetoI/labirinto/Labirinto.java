7c3f78d9ab701e868c2b904aed11a48a19e2e863
pedrogmo <42345539+pedrogmo@users.noreply.github.com>
2018-09-26 13:47:56 -0300

    Adionado arquivo teste2.txt

--- 18015_18174_ProjetoI/labirinto/Labirinto.java
+++ 18015_18174_ProjetoI/labirinto/Labirinto.java
@@ -1,4 +1,4 @@
-ï»¿import java.io.InputStreamReader;
+import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -24,7 +24,7 @@ public class Labirinto
 			Pilha<Coordenada> caminho = new Pilha<Coordenada>(colunas * linhas);
 			Pilha<Fila<Coordenada>> possibilidades = new Pilha<Fila<Coordenada>>(colunas * linhas);
 			Coordenada atual = null;
-			//leitura do arquivo texto, atribuiÃ§Ã£o dos valores da matriz
+			//leitura do arquivo texto, atribuição dos valores da matriz
 			for (int l = 0; l < linhas; l++)
 			{
 				String linha = arq.readLine();
@@ -33,19 +33,19 @@ public class Labirinto
 					char valoratual = linha.charAt(c);
 					//System.out.print(valoratual + " ");
 					matriz[l][c] = valoratual;
-					if (c==0 || l==0 || c== colunas - 1 || l == linhas - 1) //se Ã© a borda do labirinto
-						if (valoratual == 'E') //se o caractere lido Ã© 'E', inÃ­cio do labirinto
+					if (c==0 || l==0 || c== colunas - 1 || l == linhas - 1) //se é a borda do labirinto
+						if (valoratual == 'E') //se o caractere lido é 'E', início do labirinto
 							atual = new Coordenada(l,c);
 				}
 				System.out.println();
 			}
 			arq.close();
 			if (atual == null)
-				throw new Exception("Caractere 'E' do inÃ­cio do labirinto nÃ£o econtrado.");
+				throw new Exception("Caractere 'E' do início do labirinto não econtrado.");
 			System.out.println("Comeco do labirinto: " + atual.toString());
 
 			Fila<Coordenada> fila = null; //fila de coordenadas armazenadas e possivelmente tomadas
-			//trÃªs variÃ¡veis boolean para controle dos loops e dos desvios de cÃ³digo:
+			//três variáveis boolean para controle dos loops e dos desvios de código:
 			boolean acabou = false;
 			boolean passouRegressivo = false;
 			boolean sairRegressivo = false;
@@ -55,7 +55,7 @@ public class Labirinto
 				if (!passouRegressivo)
 				{
 					fila = new Fila<Coordenada>(3);
-					//colocar as coordenadas possÃ­veis de movimento na fila
+					//colocar as coordenadas possíveis de movimento na fila
 					int cAtual = atual.getColuna();
 					int lAtual = atual.getLinha();
 					if (cAtual + 1 < colunas)         //para direita
@@ -73,14 +73,14 @@ public class Labirinto
 				}
 				else //passou regressivo
 					passouRegressivo = false; //voltou para modo progressivo
-				if (!fila.isVazia()) //hÃ¡ lugar para ir, modo progressivo
+				if (!fila.isVazia()) //há lugar para ir, modo progressivo
 				{
 					System.out.println("\nModo progressivo");
 					atual = fila.getUmItem();
 					fila.jogueForaUmItem();
 					int lPasso = atual.getLinha();
 					int cPasso = atual.getColuna();
-					if (matriz[lPasso][cPasso] == 'S') //achou saÃ­da
+					if (matriz[lPasso][cPasso] == 'S') //achou saída
 					{
 						System.out.println("\n\nLabirinto resolvido.");
 						acabou = true;
@@ -98,16 +98,16 @@ public class Labirinto
 						}
 						System.out.println("\nSaida: " + atual.toString());
 					}
-					else //nÃ£o Ã© o saÃ­da, Ã© sÃ³ um espaÃ§o em branco. Tem que dar passo
+					else //não é o saída, é só um espaço em branco. Tem que dar passo
 					{
 						matriz[lPasso][cPasso] = '*'; //dar passo
 						printaMatriz(matriz, linhas, colunas);
 						caminho.guarde(atual);
 						possibilidades.guarde(fila);
-						//printa matriz a cada repetiÃ§Ã£o, para visualizaÃ§Ã£o
+						//printa matriz a cada repetição, para visualização
 					}
 				}
-				else //nÃ£o hÃ¡ lugar para ir, modo regressivo
+				else //não há lugar para ir, modo regressivo
 				{
 					passouRegressivo = true;
 					sairRegressivo = false;
@@ -124,7 +124,7 @@ public class Labirinto
 						if (!fila.isVazia()) //fila anterior recuperada tem coordenadas para ir
 							sairRegressivo = true;
 						if (possibilidades.isVazia()) //se se essgotaram asss possibilidades
-							throw new Exception("NÃ£o foi possÃ­vel resolver o labirinto");
+							throw new Exception("Não foi possível resolver o labirinto");
 					}
 					while (!sairRegressivo);
 				}
