e2cd25342fc6da7b8dd803dc06769069bde03a82
GustavoHMeira <cc18015@g.unicamp.br>
2018-09-28 08:16:18 -0300

    O p do OutputStream é minusc

--- 18015_18174_ProjetoI/programa/Programa.java
+++ 18015_18174_ProjetoI/programa/Programa.java
@@ -1,7 +1,7 @@
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.OutPutStream;
+import java.io.OutputStream;
 import java.io.PrintStream;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -16,10 +16,10 @@ public class Programa
 	public static void main(String[] args)
 	{
 		try
-		{			
+		{
 			BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
 			System.out.print("Digite o local e o nome do arquivo a ser lido, mais sua extensao .txt: ");
-			String localArquivo = teclado.readLine();			
+			String localArquivo = teclado.readLine();
 			BufferedReader arq = new BufferedReader(new FileReader(localArquivo));
 			int linhas = Integer.parseInt(arq.readLine().trim());
 			int colunas = Integer.parseInt(arq.readLine().trim());
@@ -30,17 +30,17 @@ public class Programa
 				String linha = arq.readLine();
 				for (int c = 0; c < colunas; c++)
 				{
-					char valoratual = linha.charAt(c);				
+					char valoratual = linha.charAt(c);
 					matriz[l][c] = valoratual;
 				}
 			}
 			arq.close();
 			Labirinto lab = new Labirinto(matriz, linhas, colunas);
-			lab.procurarSaida();			
+			lab.procurarSaida();
 			Coordenada saida = lab.getSaida();
 			System.out.println("Labirinto resolvido.");
 			System.out.println("Caminho: " + lab.getCaminhoFinal());
-			System.out.println("Saida em: " + saida.toString());			
+			System.out.println("Saida em: " + saida.toString());
 			String arqSaida = localArquivo.substring(0, localArquivo.length() - 4) + ".res.txt";
 			PrintStream resultado = new PrintStream(arqSaida);
 			resultado.println(lab.toString());
